ec466d29dfb29a6d62b449cac668c3f747c0c018
Leonardo Reina <leo.reina01@gmail.com>
2022-04-07 12:00:40 -0300

    finishing refactoring Food, Header and Input components

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,8 +9,16 @@ import { useField } from '@unform/core';
 
 import { Container } from './styles';
 
-const Input = ({ name, icon: Icon, ...rest }) => {
-  const inputRef = useRef(null);
+interface InputProps {
+  name: string;
+  Icon: any;
+  rest: {
+    placeholder: string;
+  }
+}
+
+export default function Input({ name, Icon, ...rest }: InputProps) {
+  const inputRef: { current: any } = useRef(null);
 
   const [isFocused, setIsFocused] = useState(false);
   const [isFilled, setIsFilled] = useState(false);
@@ -49,5 +57,3 @@ const Input = ({ name, icon: Icon, ...rest }) => {
     </Container>
   );
 };
-
-export default Input;
